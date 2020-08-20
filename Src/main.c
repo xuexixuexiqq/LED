e6c131f908839b6e28312a6f66057f4b59ed0f4c
xuexixuexiqq <273894953@qq.com>
2020-08-20 16:06:10 +0800

    在 main.c 中添加 LED 闪烁

--- Src/main.c
+++ Src/main.c
@@ -99,7 +99,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_Delay(500);
+    HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
